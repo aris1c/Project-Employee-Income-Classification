c72b3185ed0ed308c413e8e23a2d303237eb9ad3
Aris Candra <134421714+aris1c@users.noreply.github.com>
2025-09-01 01:17:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
         st.markdown(
             """
             <div style="max-width: 900px; margin: auto; font-size:18px; line-height:1.6; text-align: justify;">
-                Ini adalah website deploy by Alya Devi Anjani untuk mengklasifikasikan pendapatan seseorang ke dalam dua kategori, yaitu pendapatan kurang dari atau sama dengan 50 ribu dolar (<=50K) dan lebih dari 50 ribu dolar (>50K). Prediksi ini didasarkan pada berbagai atribut demografis dan pekerjaan seperti usia, jenis pekerjaan, tingkat pendidikan, status pernikahan, ras, jenis kelamin, jam kerja per minggu, serta informasi finansial seperti keuntungan atau kerugian modal. Dengan menggunakan data tersebut, model dapat membantu memperkirakan tingkat pendapatan seseorang berdasarkan karakteristik individu tersebut.
+                Ini adalah website deploy by Aris Candra untuk mengklasifikasikan pendapatan seseorang ke dalam dua kategori, yaitu pendapatan kurang dari atau sama dengan 50 ribu dolar (<=50K) dan lebih dari 50 ribu dolar (>50K). Prediksi ini didasarkan pada berbagai atribut demografis dan pekerjaan seperti usia, jenis pekerjaan, tingkat pendidikan, status pernikahan, ras, jenis kelamin, jam kerja per minggu, serta informasi finansial seperti keuntungan atau kerugian modal. Dengan menggunakan data tersebut, model dapat membantu memperkirakan tingkat pendapatan seseorang berdasarkan karakteristik individu tersebut.
             </div>
             """,
             unsafe_allow_html=True
